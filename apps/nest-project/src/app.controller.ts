ea289b2d32f86c1e98810abfe89548aa7471935e
chrisdeguara <chris.deguara@handsonsystems.com>
2023-08-14 17:23:08 +0200

    Clean imports

--- apps/nest-project/src/app.controller.ts
+++ apps/nest-project/src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
-import { CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 
 @Controller()
 @UseInterceptors(CacheInterceptor)
